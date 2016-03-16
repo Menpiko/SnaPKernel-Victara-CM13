38d8fec66767dc08a2a0577eecd7f0887dc97884
mennpiko <mennpiko@gmail.com>
2016-03-16 11:12:48 +0100

    Gov-SLIM

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -383,6 +383,9 @@ extern struct cpufreq_governor cpufreq_gov_nightmare;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SLIM)
+extern struct cpufreq_governor cpufreq_gov_slim;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_slim)
 #endif
 
 /*********************************************************************
